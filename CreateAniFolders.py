1ecd92c1b87cd64675589c88a6f86efb6a838521
easylife122 <easylife122@gmail.com>
2023-03-27 08:23:43 +0800

    Update

--- CreateAniFolders.py
+++ CreateAniFolders.py
@@ -7,12 +7,27 @@ import ShotGridInterface
 c_query = ShotGridInterface.query
 
 class FolderAniCreator():
+    def __init__(self):
+        super().__init__()
+
+        self.sf01Env_path = ''
+        self.sf02Ch_path = ''
+        self.sf03Seq_path = ''
+        self.sef04Temp_path = ''
+
     def create_folders(self, top_folder_entry):
 
+        # Create folder to destination
         target_dir = 'D:/Projects/UEProjectTemp'
+
+        # A folder directory for copying UE sample project
         UE_source_folder = "D:/Projects/UnreaProjects/ExampleUE_5_1_1"
-        UEContent_folder_name = 'Content'
+
+        # Define the UE sample project .uproject name
         example_uproject_name = 'ExampleUE_5_1_1.uproject'
+
+        # Inside UE project define mother folder
+        UEContent_folder_name = 'Content'
 
         # Get the current date
         today = datetime.date.today()
@@ -20,17 +35,17 @@ class FolderAniCreator():
         # Format the date as YYYY-MM-DD
         date_str = today.strftime('%Y%m')
 
-        # get input values from text fields
-        top_folder_entry_remove_space = top_folder_entry.replace(" ", "_")
+        # Get input values from text fields and remove space and replace to '_'
+        top_folder_entry_remove_colon = top_folder_entry.replace(':', "")
+        top_folder_entry_remove_space = top_folder_entry_remove_colon.replace(" ", "_")
         top_folder_name = top_folder_entry_remove_space
         date_top_folder_name = date_str + '_' + top_folder_name
 
-        # 2 lvl Folder names
+        # Define 2nd lvl Folder names
         sub_folder_01Env = '01_Env'
         sub_folder_02Ch = '02_Ch'
         sub_folder_03Seq = '03_Seq'
         sub_folder_04Temp = '04_Temp'
-
 
         # create the target directory if it doesn't already exist
         if not os.path.exists(target_dir):
@@ -45,22 +60,31 @@ class FolderAniCreator():
         new_folder_path = target_dir + '/' + new_folder_name
         shutil.move(copied_folder_path, new_folder_path)
 
-        # create level 2 subfolder inside the top level folder
-        os.makedirs(os.path.join(target_dir, date_top_folder_name, UEContent_folder_name, sub_folder_01Env))
-        os.makedirs(os.path.join(target_dir, date_top_folder_name, UEContent_folder_name, sub_folder_02Ch))
-        os.makedirs(os.path.join(target_dir, date_top_folder_name, UEContent_folder_name, sub_folder_03Seq))
-        os.makedirs(os.path.join(target_dir, date_top_folder_name, UEContent_folder_name, sub_folder_04Temp))
-
-        # create level 3 subfolder inside 03Seq
-        # sequences = c_query.shotgridData()
-
-
-        # rename uproject
+        # rename .uproject
         old_uproject_path_name = new_folder_path +'/' + example_uproject_name
         new_uproject_name = top_folder_entry_remove_space + '.uproject'
         new_uproject_path_name = new_folder_path + '/' + new_uproject_name
         os.rename(old_uproject_path_name, new_uproject_path_name)
         print(old_uproject_path_name, new_uproject_path_name)
+
+        # Create level 2 subfolder path
+        self.sf01Env_path = os.path.join(target_dir, date_top_folder_name, UEContent_folder_name, sub_folder_01Env)
+        self.sf02Ch_path = os.path.join(target_dir, date_top_folder_name, UEContent_folder_name, sub_folder_02Ch)
+        self.sf03Seq_path = os.path.join(target_dir, date_top_folder_name, UEContent_folder_name, sub_folder_03Seq)
+        self.sef04Temp_path = os.path.join(target_dir, date_top_folder_name, UEContent_folder_name, sub_folder_04Temp)
+
+        # create level 2 subfolders
+        os.makedirs(self.sf01Env_path)
+        os.makedirs(self.sf02Ch_path)
+        os.makedirs(self.sf03Seq_path)
+        os.makedirs(self.sef04Temp_path)
+
+
+
+  # def create_subfolders(self):
+
+        # create level 3 subfolder inside 03Seq
+       #  sequences = c_query.shotgridData()
 
 if __name__ == '__main__':
 
@@ -68,6 +92,7 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     folder_creator = FolderAniCreator()
     folder_creator.show()
+
 
     # start the event loop
     sys.exit(app.exec_())
